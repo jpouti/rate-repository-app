b496615ec157784a0a5cf3863512826002ebba9a
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-10-25 19:01:58 +0300

    10.4

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,28 @@
+import { View, StyleSheet, Pressable } from 'react-native';
+import Text from './text'
+import Constants from 'expo-constants';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    paddingLeft: 25,
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: '#24292',
+  },
+  flexItemA: {
+    paddingTop: 40,
+    paddingBottom: 25,
+    flexGrow: 1,
+  },
+});
+
+const AppBar = () => {
+  return <View style={styles.container}>
+            <Pressable style={styles.flexItemA}>
+                <Text fontWeight="bold" fontSize="subheading" color='white' style={styles.textOver}>Repositories</Text>
+            </Pressable>
+        </View>;
+};
+
+export default AppBar;
